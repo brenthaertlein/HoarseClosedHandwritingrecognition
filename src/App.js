e4eaddde5ff72fa031166cb7479272a6b68262b6
Brent Haertlein <11946622+brenthaertlein@users.noreply.github.com>
2021-09-08 03:05:43 +0000

    new logo! more birb!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" style={{color: "white"}} />
+        <h1>
+          Bird Generator!
+        </h1>
       </header>
+      <div class="footer">
+        <p>
+          Written in&nbsp;
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
